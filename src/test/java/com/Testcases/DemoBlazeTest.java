00b61a0d081898934f5f5526d865ae63e3f50a4a
*aishwaryaa* <*aishushettar9902@gmail.com*>
2024-02-02 16:56:11 +0530

    This is my commit

--- src/test/java/com/Testcases/DemoBlazeTest.java
+++ src/test/java/com/Testcases/DemoBlazeTest.java
@@ -4,15 +4,12 @@ import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-
-import com.aventstack.extentreports.ExtentReports;
-import com.aventstack.extentreports.reporter.ExtentSparkReporter;
-
 import BaseTests.BaseTestClass;
 import QAPages.HomePage;
 import QAPages.ProductPage;
 import UtilsTests.TestUtil;
-     // Author is Aishwarya Nandishettar
+     
+   // Authow Aishwarya Nandishettar
 public class DemoBlazeTest extends BaseTestClass
   {
 	
